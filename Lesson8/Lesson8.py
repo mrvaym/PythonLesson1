a6b00245a9d0faa23e471c1809783caff4fb2400
mr.vaym <mr.vaym@gmail.com>
2020-02-03 16:30:39 +0300

    Lesson8 tracemalloc

--- Lesson8/Lesson8.py
+++ Lesson8/Lesson8.py
@@ -1,51 +1,79 @@
 import datetime
-
+import tracemalloc
 from sys import getsizeof
 
-def show_time (f):
-    def wrapper(*args,**kwargs):
+
+def show_time(f):
+    def wrapper(*args, **kwargs):
         start_time = datetime.datetime.now()
-        a=f(*args, **kwargs)
+        a = f(*args, **kwargs)
         finish_time = datetime.datetime.now()
         report_time = finish_time - start_time
-        print(report_time)
+        print(f'Время выполнения:{report_time}')
         return a
+
     return wrapper
 
-def show_memory (f):
-    def wrapper(*args,**kwargs):
-        a=f(*args, **kwargs)
+
+def show_memory(f):
+    def wrapper(*args, **kwargs):
+        a = f(*args, **kwargs)
         memory_size = getsizeof(a)
-        print(memory_size)
+        print(f'Данные в памяти: {memory_size} байт')
         return a
+
+    return wrapper
+
+
+def show_memory_2(f):
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+        a = f(*args, **kwargs)
+        memory_size = tracemalloc.get_traced_memory()
+        print(f'Использование памяти:{memory_size[0]}байт MAX:{memory_size[1]}байт')
+        tracemalloc.stop()
+        return a
+
     return wrapper
 
 
 @show_time
 @show_memory
-
+@show_memory_2
 def list_num(a):
-    num_list=[x for x in range(1,a+1) ]
+    print('Генератор списка ------------ ')
+    num_list = [x for x in range(a)]
     return num_list
+
 
 @show_time
 @show_memory
-
+@show_memory_2
 def gen_num(a):
-    num_list=(x for x in range(1,a+1) )
+    print('Неявный генератор ------------ ')
+    num_list = (x for x in range(1, a + 1))
     return num_list
+
 
 @show_time
 @show_memory
-
+@show_memory_2
 def gen_num2(a):
+    print('Явный генератор ------------ ')
     for x in range(a):
         x += 1
         yield x
 
 
-a=list_num(100000)
+a = list_num(100000)
 
-b=gen_num(100000)
+b = gen_num(100000)
+print(next(b))
+# for i in b:
+#     print(i)
 
-c=gen_num2(100000)
+c = gen_num2(100000)
+print(next(c))
+#
+# for i in c:
+#     print(i)
